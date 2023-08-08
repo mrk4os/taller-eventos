3a06e7c963f0973d18049f593059e5506c58e558
Marcos Araujo <marcos_4raujo@outlook.com>
2023-08-08 02:17:19 -0300

    div alert

---
+++ eventos.js
@@ -0,0 +1,8 @@
+
+const divAlert = document.getElementById("divAlert");
+
+//Manejador de eventos (Event handler para los que saben ingles)
+
+divAlert.addEventListener("click", function () {
+    alert("Hola! Soy el div");
+});

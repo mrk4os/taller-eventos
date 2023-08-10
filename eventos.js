bcaa9b94b03e84712c6251f1d1918afb78de8f25
Marcos Araujo <marcos_4raujo@outlook.com>
2023-08-09 22:37:15 -0300

    Arreglo alertas

--- eventos.js
+++ eventos.js
@@ -1,8 +1,13 @@
 
 const divAlert = document.getElementById("divAlert");
+const saludo = document.getElementById("hola");
 
-//Manejador de eventos (Event handler para los que saben ingles)
+// Manejador de eventos (Event handler para los que saben ingles)
 
-divAlert.addEventListener("click", function () {
-    alert("Hola! Soy el div");
-});
+    divAlert.addEventListener("click", function () {
+        alert("Hola! Soy el div");
+    });
+// Arreglo a las alertas saltando a la vez.
+    saludo.addEventListener("click", function(event) {
+        event.stopPropagation();
+    })
